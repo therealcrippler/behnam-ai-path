2eba003a16737332ee102b608fc7626eb797984a
therealcrippler <ai.behnam10@gmail.com>
2025-05-19 17:44:59 +0330

    day2 - متغیرها و محاسبه سن مادر موقع تولد

---
+++ Day2.py
@@ -0,0 +1,7 @@
+your_name = "behnam"
+your_age = 37
+mother_age = 55
+birth_age = mother_age - your_age
+print("my mom when i was born" + str(birth_age) + "years old she was")
+print("My mom was " + str(birth_age) + " years old when I was born.")
+print("My mom was " + str(birth_age) + " years old when i was born.")
